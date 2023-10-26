fb476207586220a59f7965e2dd53d63b3764fba4
John Mac <jcmacmi@gmail.com>
2023-10-25 22:48:45 -0400

    Sorted out gray code solution

--- codes.ml
+++ codes.ml
@@ -1,5 +1,13 @@
+let binary_to_gray n =
+  n lxor (n lsr 1)
 
+let rec binary_digits_string n m res =
+  if m = 0 then res 
+  else binary_digits_string (n lsr 1) (m-1) ((string_of_int (n land 1)) ^ res)
 
-let rec gray_aux n = 
-  if n = 1 then ["0" ; "1"]
-  else List.append (gray_aux n-1) ["1"]
+let rec gray_code_aux n m res = 
+  if m = (1 lsl n) then res
+  else gray_code_aux n (m+1) ((binary_digits_string (binary_to_gray m) n "") :: res)  
+
+let gray_code n =
+  List.rev (gray_code_aux n 0 [])
